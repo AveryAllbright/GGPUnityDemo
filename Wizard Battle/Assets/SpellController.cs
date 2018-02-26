2a16d67b8234419b17e4802760d584207a1314ff
THEEVERNET\VaanEverick <cdb6474@g.rit.edu>
2018-02-26 01:06:28 -0500

    First Push of the Unity demo. the basics are done. Collision resolution and some additional behaviours are all that's really left to do.

---
+++ Wizard Battle/Assets/SpellController.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpellController : MonoBehaviour {
+
+    GameObject target;
+    GameObject user;
+    Vector3 direction;
+    public int lifeSpan;
+    float speed;
+    int damage;
+
+    public string Tag;
+
+	// Use this for initialization
+	void Start ()
+    {
+
+        switch (Tag)
+        {
+            case "FireBall": lifeSpan = 100; speed = 12f; damage = 8; direction = user.transform.forward; transform.position = user.transform.position + user.transform.forward; break;
+            case "MagicMissile": lifeSpan = 250; speed = 8f; damage = 3; break;
+            case "RockWall": lifeSpan = 500; break;
+        }
+    }
+	
+	// Update is called once per frame
+	void Update ()
+    {
+		switch(Tag)
+            {
+            case "FireBall": Fireball(); break;
+            case "MagicMissile": MagicMissile(); break;
+            case "RockWall": RockWall(); break;
+        }
+	}
+
+    void Fireball()
+    {
+        transform.position += direction / speed;
+        lifeSpan--;
+        
+    }
+
+    void MagicMissile()
+    {
+
+    }
+
+    void RockWall()
+    {
+
+    }
+
+    public void SetUser(GameObject a_user)
+    {
+        user = a_user;
+    }
+}
---
+++ Wizard Battle/Assets/SpellManager.cs
@@ -0,0 +1,74 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpellManager : MonoBehaviour
+{
+
+    public GameObject FireBallPrefab;
+    public GameObject MagicMissilePrefab;
+    public GameObject RockWallPrefab;
+
+   public  GameObject user;
+
+    GameObject fireBall;
+    GameObject magicMissile;
+    GameObject rockWall;
+
+
+    int m_nActiveSpell = 0;
+    int m_nSpellCount = 2;
+
+    public List<GameObject> LiveSpells;
+    public List<GameObject> SpellBook;
+
+    void Start()
+    {
+         fireBall = Instantiate(FireBallPrefab);
+         magicMissile = Instantiate(MagicMissilePrefab);
+         rockWall = Instantiate(RockWallPrefab);
+
+        SpellBook.Add(fireBall);
+        SpellBook.Add(magicMissile);
+        SpellBook.Add(rockWall);
+
+        user = GameObject.Find("FPSController");
+       
+    }
+
+
+    void Update()
+    {
+        var input = (Input.GetAxis("Mouse ScrollWheel"));
+        if (input > 0f)
+        {
+            m_nActiveSpell++;
+            {
+                if (m_nActiveSpell > m_nSpellCount) { m_nActiveSpell = 0; }
+            }
+        }
+        else if (input < 0f)
+        {
+            m_nActiveSpell--;
+            if (m_nActiveSpell < 0) { m_nActiveSpell = m_nSpellCount; }
+        }
+
+        if (Input.GetButtonUp("Fire1")) 
+        {
+            LiveSpells.Add(Instantiate(SpellBook[m_nActiveSpell]));
+            LiveSpells[LiveSpells.Count - 1].GetComponent<SpellController>().SetUser(user);
+        }
+
+        foreach (GameObject spell in LiveSpells)
+        {
+            SpellController con = spell.GetComponent<SpellController>();
+            if(con.lifeSpan <=0)
+            {
+                Destroy(spell);
+            }
+        }
+
+        
+
+    }
+}
